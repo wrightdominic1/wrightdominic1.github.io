eeeaee7cf4eff6d2146f864e4c936cf1c3381cf3
Dominic Wright <wrightdominic138@yahoo.com>
2019-03-27 15:11:03 -0500

    cleaning.

--- burger/db/seeds.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) 
-VALUES ('Bacon Bluecheese', false);
-
-INSERT INTO burgers (burger_name, devoured) 
-VALUES ('McChicken', false);
-
-INSERT INTO burgers (burger_name, devoured) 
-VALUES ('Matt Special', false);
